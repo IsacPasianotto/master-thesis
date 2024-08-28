a1986ed0da369671bb2817fef31692f1473e817d
IsacPasianotto <yh4mop12z@gmail.com>
2024-08-28 19:19:56 +0200

    fixed the code to plot
    plot with same ylim and a plot dedicated to latency

--- chpt3-osu/out/plotter.py
+++ chpt3-osu/out/plotter.py
@@ -32,6 +32,9 @@ FIG_SIZE: Tuple[int, int] = (9, 6)
 DATA_FILE: str = './processed_data_aggregated.csv'
 PLOTS_DIR: str = 'plots'
 
+BANDWIDTH_YLIM: Tuple[float, int] = (0.01, 31000)
+LATENCY_YLIM:   Tuple[float, int] = (0.1, 8500)
+
 ########
 ## Plot function
 ########
@@ -44,8 +47,9 @@ def make_plot(
         file_to_save: str = "",
         statistics:   str = 'Mean',
         logscale:     bool = True,
-        variances:    bool = False
-    ) -> None:
+        variances:    bool = False,
+        ylim:          Tuple[float, int] = None
+    )-> None:
 
     """
     Make a plot of the benchmark data contained in a given DataFrame.
@@ -68,7 +72,8 @@ def make_plot(
         If True, the plot will be displayed in log scale. Default is True.
     variances : bool, optional
         If True, the plot will include variance bands. Default is False.
-
+    ylim : Tuple[float, int], optional
+        Tuple with the limits of the y-axis. Default is None. If not provided, the limits will be set according to the benchmark.
     Returns
     -------
     None
@@ -105,10 +110,14 @@ def make_plot(
         plt.fill_between(sizes, calico_lower, calico_upper, color=COLORS[2], alpha=ALPHA)
         plt.fill_between(sizes, cilium_lower, cilium_upper, color=COLORS[3], alpha=ALPHA)
 
+
     # Set logarithmic scale if requested
     if logscale:
         plt.yscale('log')
         plt.xscale('log')
+
+    if ylim:
+        plt.ylim(ylim)
 
     if logscale:
         plt.xlabel(r'$\log{\left(\# \textrm{ of texttt{MPI\_CHAR} sent}\right)}$', fontsize = XLABEL_FONTSIZE, labelpad = XLABEL_PAD)
@@ -135,6 +144,82 @@ def make_plot(
     else:
         # no file to save, show it
         plt.show()
+
+
+def make_latency_plot(
+        data:         pd.DataFrame,
+        cni:          str  = "cilium",
+        title:        str  = "",
+        logscale:     bool =  True,
+        statistics:   str  = 'Mean',
+        file_to_save: str  = ""
+    ) -> None:
+    """
+    Make a plot to compare how the latency of a given CNI plugin acts in
+    one or two nodes.
+
+   Parameters
+    ----------
+    data : pd.DataFrame
+        DataFrame containing the benchmark data.
+    cni: str
+        Cni to use. Supported values are "cilium", "calico" and "flannel"
+    title : str
+        Title of the plot.
+    file_to_save : str, optional
+        Name of the file to save the plot. If not provided, the plot will be displayed.
+    statistics : str, optional
+        Statistics to plot. Default is 'Mean'.
+    logscale : bool, optional
+        If True, the plot will be displayed in log scale. Default is True.
+    file_to_save: str
+        File name to save the obtained plot. if empty the plot will not be saved but displayed.
+    Returns
+    -------
+    None
+
+    """
+
+    # Extract sizes for x-axis
+    sizes: np.ndarray = data['Size'].unique()
+
+    cni: pd.DataFrame = data[data['CNI'] == cni]
+    one: np.ndarray = cni[cni['Nodes'] == 1][statistics].values
+    two: np.ndarray = cni[cni['Nodes'] == 2][statistics].values
+
+    # Plotting
+    plt.plot(sizes, one, label = "1 node", color = COLORS[0], marker = 's')
+    plt.plot(sizes, two, label = "2 nodes",color = COLORS[1], marker = 'D')
+
+    if logscale:
+        plt.xscale('log')
+        plt.yscale('log')
+
+    if logscale:
+        plt.xlabel(r'$\log{\left(\# \textrm{ of texttt{MPI\_CHAR} sent}\right)}$', fontsize = XLABEL_FONTSIZE, labelpad = XLABEL_PAD)
+        plt.ylabel(r'$\log{\left(\textrm{ Latency }\right)}\quad\left[\log{\left( \mu s \right)}\right]$', fontsize = YLABEL_FONTSIZE, labelpad = YLABEL_PAD)
+
+    else:
+        plt.xlabel(r'\# of \texttt{MPI\_CHAR} sent', fontsize = XLABEL_FONTSIZE, labelpad = XLABEL_PAD)
+        plt.ylabel(r'Latency $\quad\left[\mu s\right]$', fontsize = YLABEL_FONTSIZE, labelpad = YLABEL_PAD)
+
+    # Access the current axes
+    ax = plt.gca()
+    # Hide the top and right spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    plt.minorticks_off()
+    plt.title(title, fontsize=TITLE_FONTSIZE, pad= TITLE_PAD)
+    plt.legend(loc ='best', fontsize=LEGEND_FONTSIZE, frameon=False)
+
+    if file_to_save:
+        plt.savefig(PLOTS_DIR + '/' + file_to_save)
+        plt.close()
+    else:
+        # no file to save, show it
+        plt.show()
+
 
 ########
 ## Main
@@ -172,21 +257,25 @@ def main():
     # -- Make plots --
     os.makedirs(PLOTS_DIR, exist_ok=True)
 
-    make_plot(latency_1, '\\texttt{osu_latency} -- Pods on the same node', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency-1-node.pdf', logscale=True)
-    make_plot(latency_2, '\\texttt{osu_latency} -- Pods on 2 different nodes', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency-2-nodes.pdf', logscale=True)
-    make_plot(latency_mp_1, '\\texttt{osu_latency} -- Pods on the same node', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency_mp-1-node.pdf', logscale=True)
-    make_plot(latency_mp_2, '\\texttt{osu_latency} -- Pods on 2 different nodes', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency_mp-2-nodes.pdf', logscale=True)
-    make_plot(multi_latency_1, '\\texttt{osu_multi_lat} -- Pods on the same node', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'multi-latency-1-node.pdf', logscale=True)
-    make_plot(multi_latency_2, '\\texttt{osu_multi_lat} -- Pods on 2 different nodes', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'multi-latency-2-nodes.pdf', logscale=True)
+    make_plot(latency_1, '\\texttt{osu_latency} -- Pods on the same node', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency-1-node.pdf', logscale=True, ylim=LATENCY_YLIM)
+    make_plot(latency_2, '\\texttt{osu_latency} -- Pods on 2 different nodes', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency-2-nodes.pdf', logscale=True, ylim=LATENCY_YLIM)
+    make_plot(latency_mp_1, '\\texttt{osu_latency_mp} -- Pods on the same node', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency_mp-1-node.pdf', logscale=True, ylim=LATENCY_YLIM)
+    make_plot(latency_mp_2, '\\texttt{osu_latency_mp} -- Pods on 2 different nodes', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'latency_mp-2-nodes.pdf', logscale=True, ylim=LATENCY_YLIM)
+    make_plot(multi_latency_1, '\\texttt{osu_multi_lat} -- Pods on the same node', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'multi-latency-1-node.pdf', logscale=True, ylim=LATENCY_YLIM)
+    make_plot(multi_latency_2, '\\texttt{osu_multi_lat} -- Pods on 2 different nodes', ylabel='Latency', unit_y_label = r'\mu s', file_to_save = 'multi-latency-2-nodes.pdf', logscale=True, ylim=LATENCY_YLIM)
 
-    make_plot(bw_1, '\\texttt{osu_bw} -- Pods on the same node', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bw-1-node.pdf', logscale=True)
-    make_plot(bw_2, '\\texttt{osu_bw} -- Pods on 2 different nodes', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bw-2-nodes.pdf', logscale=True)
-    make_plot(bibw_1, '\\texttt{osu_bibw} -- Pods on the same node', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bibw-1-node.pdf', logscale=True)
-    make_plot(bibw_2, '\\texttt{osu_bibw} -- Pods on 2 different nodes', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bibw-2-nodes.pdf', logscale=True)
-    make_plot(mbw_mr_1, '\\texttt{osu_mbw_mr} -- Pods on the same node', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'mbw_mr-1-node.pdf', logscale=True)
-    make_plot(mbw_mr_2, '\\texttt{osu_mbw_mr} -- Pods on 2 different nodes', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'mbw_mr-2-nodes.pdf', logscale=True)
+    make_plot(bw_1, '\\texttt{osu_bw} -- Pods on the same node', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bw-1-node.pdf', logscale=True, ylim=BANDWIDTH_YLIM)
+    make_plot(bw_2, '\\texttt{osu_bw} -- Pods on 2 different nodes', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bw-2-nodes.pdf', logscale=True, ylim=BANDWIDTH_YLIM)
+    make_plot(bibw_1, '\\texttt{osu_bibw} -- Pods on the same node', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bibw-1-node.pdf', logscale=True, ylim=BANDWIDTH_YLIM)
+    make_plot(bibw_2, '\\texttt{osu_bibw} -- Pods on 2 different nodes', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'bibw-2-nodes.pdf', logscale=True, ylim=BANDWIDTH_YLIM)
+    make_plot(mbw_mr_1, '\\texttt{osu_mbw_mr} -- Pods on the same node', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'mbw_mr-1-node.pdf', logscale=True, ylim=BANDWIDTH_YLIM)
+    make_plot(mbw_mr_2, '\\texttt{osu_mbw_mr} -- Pods on 2 different nodes', ylabel='Bandwidth', unit_y_label = r'MB/s', file_to_save = 'mbw_mr-2-nodes.pdf', logscale=True, ylim=BANDWIDTH_YLIM)
 
-    print('Plots saved in the' + PLOTS_DIR + ' directory.')
+    make_latency_plot(latency, title='\\texttt{osu_latency} -- Cilium CNI Plugin', file_to_save="cilium-latency.pdf" )
+    make_latency_plot(latency_mp, title='\\texttt{osu_latency_mp} -- Cilium CNI Plugin', file_to_save='cilium-latency_mp.pdf')
+    make_latency_plot(multi_latency, title='\\texttt{osu_multi_lat} -- Cilium CNI Plugin', file_to_save='cilium-multi-latency.pdf')
+
+    print('Plots saved in the ' + PLOTS_DIR + ' directory.')
 
 if __name__ == '__main__':
     main()
